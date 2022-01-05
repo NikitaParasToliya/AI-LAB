43f073654bb07084060aed4ec909fbbb40d93f56
NikitaParasToliya <71306370+NikitaParasToliya@users.noreply.github.com>
2022-01-05 09:28:51 +0530

    Update 8 Puzzle.py

--- LAB2/8 Puzzle.py
+++ LAB2/8 Puzzle.py
@@ -1,137 +1,143 @@
-board = [' ' for x in range(10)]
+def h(state):
+    res = 0
+    for i in range(1, 9):
+        if state.index(i) != target.index(i):
+            res += 1
+    return res
 
 
-def insertLetter(letter, pos):
-    board[pos] = letter
+def gen(state, m, b):
+    temp = state[:]
+    if m == "l":
+        temp[b], temp[b - 1] = temp[b - 1], temp[b]
+    if m == "r":
+        temp[b], temp[b + 1] = temp[b + 1], temp[b]
+    if m == "u":
+        temp[b], temp[b - 3] = temp[b - 3], temp[b]
+    if m == "d":
+        temp[b], temp[b + 3] = temp[b + 3], temp[b]
+    return temp
 
 
-def spaceIsFree(pos):
-    return board[pos] == ' '
+def possible_moves(state, visited_states):
+    b = state.index(-1)
+    d = []
+    pos_moves = []
+    if b <= 5:
+        d.append("d")
+    if b >= 3:
+        d.append("u")
+    if b % 3 > 0:
+        d.append("l")
+    if b % 3 < 2:
+        d.append("r")
+    for i in d:
+        temp = gen(state, i, b)
+        if not temp in visited_states:
+            pos_moves.append(temp)
+    return pos_moves
 
 
-def printBoard(board):
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
+def search(src, target, visited_states, g):
+    if src == target:
+        return visited_states
+    visited_states.append(src),
+    adj = possible_moves(src, visited_states)
+    scores = []
+    selected_moves = []
+    for move in adj:
+        scores.append(h(move) + g)
+    min_score = min(scores)
+    for i in range(len(adj)):
+        if scores[i] == min_score:
+            selected_moves.append(adj[i])
+    for move in selected_moves:
+        if search(move, target, visited_states, g + 1):
+            return visited_states
+    return 0
 
 
-def isWinner(bo, le):
-    return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or (
-                bo[1] == le and bo[2] == le and bo[3] == le) or (bo[1] == le and bo[4] == le and bo[7] == le) or (
-                       bo[2] == le and bo[5] == le and bo[8] == le) or (
-                       bo[3] == le and bo[6] == le and bo[9] == le) or (
-                       bo[1] == le and bo[5] == le and bo[9] == le) or (bo[3] == le and bo[5] == le and bo[7] == le)
+def solve(src, target):
+    visited_states = []
+    res = search(src, target, visited_states, 0)
+
+    if type(res) != type(int()):
+        i = 0
+        for state in res:
+            display(state)
+            i += 1
+        display(target)
+        print("Total moves made: ", i + 1)
 
 
-def playerMove():
-    run = True
-    while run:
-        move = input('Please select a position to place an \'X\' (1-9): ')
-        try:
-            move = int(move)
-            if move > 0 and move < 10:
-                if spaceIsFree(move):
-                    run = False
-                    insertLetter('X', move)
-                else:
-                    print('Sorry, this space is occupied!')
-            else:
-                print('Please type a number within the range!')
-        except:
-            print('Please type a number!')
+def display(state):
+    for i in range(9):
+        if i % 3 == 0:
+            print()
+        if state[i] == -1:
+            print(state[i], end="\t")
+        else:
+            print(state[i], end="\t")
+    print(end="\n")
 
 
-def compMove():
-    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
-    move = 0
+# define source and target states
+src = [1, 2, 3, -1, 4, 5, 6, 7, 8]
+target = [1, 2, 3, 4, 5, 8, -1, 6, 7]
 
-    for let in ['O', 'X']:
-        for i in possibleMoves:
-            boardCopy = board[:]
-            boardCopy[i] = let
-            if isWinner(boardCopy, let):
-                move = i
-                return move
+def bfs(src,target):
+    queue = []
+    queue.append(src)
+   
+    exp = []
+    print("Possible Moves:")
+    while len(queue) > 0:
+        source = queue.pop(0)
+        exp.append(source)
+        print("\n")
+        print(source)
+       
+        if source==target:
+            print("\n")
+            print("Successfully solved 8 puzzle game!!")
+            return
+       
+        poss_moves_to_do = []
+        poss_moves_to_do = possible_moves(source,exp)
+       
+        for move in poss_moves_to_do:
+           
+            if move not in exp and move not in queue:
+                queue.append(move)
 
-    cornersOpen = []
-    for i in possibleMoves:
-        if i in [1, 3, 7, 9]:
-            cornersOpen.append(i)
+print("BFS/DFS method to solve 8 Puzzle")
 
-    if len(cornersOpen) > 0:
-        move = selectRandom(cornersOpen)
-        return move
-
-    if 5 in possibleMoves:
-        move = 5
-        return move
-
-    edgesOpen = []
-    for i in possibleMoves:
-        if i in [2, 4, 6, 8]:
-            edgesOpen.append(i)
-
-    if len(edgesOpen) > 0:
-        move = selectRandom(edgesOpen)
-
-    return move
-
-
-def selectRandom(li):
-    import random
-    ln = len(li)
-    r = random.randrange(0, ln)
-    return li[r]
-
-
-def isBoardFull(board):
-    if board.count(' ') > 1:
-        return False
-    else:
+print("Source State: ")
+display(src)
+print("Target State: ")
+display(target)
+print("Solving using BFS: ")               
+bfs(src, target)
+def dfs(src,target,limit,visited_states):
+    if src == target:
         return True
-
-
-def main():
-    print('Welcome to Tic Tac Toe!')
-    printBoard(board)
-
-    while not (isBoardFull(board)):
-        if not (isWinner(board, 'O')):
-            playerMove()
-            printBoard(board)
-        else:
-            print('Sorry, O\'s won this time!')
-            break
-
-        if not (isWinner(board, 'X')):
-            move = compMove()
-            if move == 0:
-                print('Tie Game!')
-            else:
-                insertLetter('O', move)
-                print('Computer placed an \'O\' in position', move, ':')
-                printBoard(board)
-        else:
-            print('X\'s won this time! Good Job!')
-            break
-
-    if isBoardFull(board):
-        print('Tie Game!')
-
-
-while True:
-    answer = input('Do you want to play again? (Y/N)')
-    if answer.lower() == 'y' or answer.lower == 'yes':
-        board = [' ' for x in range(10)]
-        print('-----------------------------------')
-        main()
-    else:
+    if limit <= 0:
+        return False
+    visited_states.append(src)
+    moves = possible_moves(src,visited_states)  
+    for move in moves:
+        if dfs(move, target, limit-1, visited_states):
+            return True
+    return False
+def iddfs(src,target,depth):
+    for i in range(depth):
+        visited_states = []
+        if dfs(src,target,i+1,visited_states):
+            return True
+    return False
+print("Solving using DFS: ")  
+for i in range(1, 100):
+    val = iddfs(src,target,i)
+    print(i, val)
+    if val == True:
         break
